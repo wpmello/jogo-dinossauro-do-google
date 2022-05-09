64a3d309b250d95b766e8090d6427a76feef5827
wpmello <wagnermello903@gmail.com>
2022-05-09 16:52:12 -0300

    Background & Game Over

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino')
 const background = document.querySelector('.background')
 let isJumping = false
+let position = 0
 
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
@@ -11,8 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-  let position = 0
-
   isJumping = true
 
   let upInterval = setInterval(() => {
@@ -37,27 +36,29 @@ function jump() {
 
 function createCactus() {
   const cactus = document.createElement('div')
-  let cactusPosition = 1000;
-  let randomTime = Math.random() * 6000;
+  let cactusPosition = 1000
+  let randomTime = Math.random() * 6000
 
   cactus.classList.add('cactus')
   cactus.style.left = 1000 + 'px'
   background.appendChild(cactus)
 
   let leftInterval = setInterval(() => {
-    
-    if(cactusPosition < -60) {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval)
+      background.removeChild(cactus)
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      // game over
       clearInterval(leftInterval);
-      background.removeChild(cactus);
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>'
     } else {
-      cactusPosition -= 10;
-      cactus.style.left = cactusPosition + 'px';
+      cactusPosition -= 10
+      cactus.style.left = cactusPosition + 'px'
     }
   }, 20)
-  
-  setTimeout(createCactus, randomTime);
-}
 
+  setTimeout(createCactus, randomTime)
+}
 
 createCactus()
 document.addEventListener('keyup', handleKeyUp)
